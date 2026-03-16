aa339b070c34dc52936c54940c6bf49d67bd29aa
fredtso <fredriquetso@gmail.com>
2026-03-16 21:01:07 +0300

    configuring pwa

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('fetch', function(event) {
-  // This is a dummy service worker to satisfy 'Installable' requirements
+const CACHE_NAME = 'paragons-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './home.html',
+  './style.css',
+  './logo.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch logic (required for PWA)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
